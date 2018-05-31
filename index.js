bfd68b7c609b82ab08bc4ee2bb7101da2d34daa7
Gideon W <gow210@stern.nyu.edu>
2018-05-31 21:16:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,15 +17,17 @@ console.log(whisper(string));
 }
 
 function sayHiToGrandma(string) {
-if (whisper(string)) {
- return ("I can\'t hear you!")
- }
- else if (shout(string)) { 
-	return ("YES INDEED!")
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
- else if ("I love you, Grandma.") {
-	return ("I love you, too.")
-}
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
 
  
